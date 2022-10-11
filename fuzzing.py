886f374b148aa002b6808983afb75ae92dfe507c
louis.verdon.etu <louis.verdon.etu@univ-lille.fr>
2022-10-11 16:51:28 +0200

    fuzzing avancé

--- fuzzing.py
+++ fuzzing.py
@@ -1,5 +1,33 @@
 import atheris
-from typing import Dict, Tuple, Union, List, Any
-import os
+import mock
+import sys
+from Main import get_correct_name, get_correct_action, get_correct_item, get_correct_quantity
 
-os.system("Main.py")
+# Test the given name in input,
+def test_get_correct_name(name):
+    with mock.patch('builtins.input', return_value=name):
+        assert get_correct_name() == ""
+
+# Test the given name in input,
+def test_get_correct_action(action):
+    with mock.patch('builtins.input', return_value=action):
+        assert get_correct_action() == ""
+
+# Test the given name in input,
+def test_get_correct_item(item):
+    with mock.patch('builtins.input', return_value=item):
+        assert get_correct_item() == ""
+
+atheris.Setup(sys.argv, test_get_correct_name)
+atheris.instrument_all();
+atheris.Fuzz()
+
+#fuzzing name
+atheris.Setup(sys.argv, test_get_correct_item)
+atheris.instrument_all();
+atheris.Fuzz()
+
+#fuzzing action
+atheris.Setup(sys.argv, test_get_correct_action)
+atheris.instrument_all();
+atheris.Fuzz()
