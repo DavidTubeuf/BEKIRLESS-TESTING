14cf7dee811fef59ab42a27032e3556058a5432c
louisverdon <78412480+louisverdon@users.noreply.github.com>
2022-10-16 18:28:26 +0200

    or'
    ;

--- fuzzing.py
+++ fuzzing.py
@@ -7,25 +7,25 @@ from Main import get_correct_name, get_correct_action, get_correct_item, get_cor
 def test_get_correct_name(name):
     with mock.patch('builtins.input', return_value=name):
         result = get_correct_name()
-        assert (result == "" || result == name)
+        assert (result == "" or result == name)
 
 # Test the given name in input,
 def test_get_correct_action(action):
     with mock.patch('builtins.input', return_value=action):
         result = get_correct_action()
-        assert (result == "" || result == action)
+        assert (result == "" or result == action)
 
 # Test the given name in input,
 def test_get_correct_item(item):
     with mock.patch('builtins.input', return_value=item):
         result = get_correct_item()
-        assert (result == "" || result == item)
+        assert (result == "" or result == item)
 
 # Test the given name in quantity,
 def test_get_correct_quantity(item):
     with mock.patch('builtins.input', return_value=quantity):
         result = get_correct_quantity()
-        assert (result == "" || result == quantity)
+        assert (result == "" or result == quantity)
 
 #fuzzing name
 atheris.Setup(sys.argv, test_get_correct_name)
