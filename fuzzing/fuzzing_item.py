f582a91120bc2117c25a1e71b4346954c264e447
louis.verdon.etu <louis.verdon.etu@univ-lille.fr>
2022-10-19 12:57:42 +0200

    fuzzing import fix

--- fuzzing/fuzzing_item.py
+++ fuzzing/fuzzing_item.py
@@ -1,6 +1,13 @@
 import atheris
 import mock
 import sys
+import os
+import inspect
+
+currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+parentdir = os.path.dirname(currentdir)
+sys.path.insert(0, parentdir) 
+
 from Main import get_correct_item
 
 # Test the given item in input,
