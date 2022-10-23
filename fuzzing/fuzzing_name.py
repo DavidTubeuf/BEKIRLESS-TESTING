b01c5ace51d717171dc63d99d1effa61f2bea726
louisverdon <78412480+louisverdon@users.noreply.github.com>
2022-10-23 18:27:39 +0200

    fuzzing name

--- fuzzing/fuzzing_name.py
+++ fuzzing/fuzzing_name.py
@@ -12,9 +12,15 @@ from Main import get_correct_name
 
 # Test the given name in input,
 def fuzzing(val):
-    with mock.patch('builtins.input', return_value=val):
-        result = get_correct_name()
-        assert (result == "" or result == val)
+    with self.assertRaises(Exception) as cm:
+        with mock.patch('builtins.input', return_value=val):
+            returned_value = get_correct_name()
+    the_exception = cm.exception
+    res = str(the_exception)
+    assert(res == "le nom de l'utilisateur ne peut pas être un nombre"
+           || res == "name can't be less than 2 letter"
+           || res == "name can't be more than 20 letter"
+           || returned_value == val)
 
 #fuzzing name
 atheris.Setup(sys.argv, fuzzing)
