5035749d5e7fa6a769524c9877ed3d3c546e5a94
louisverdon <78412480+louisverdon@users.noreply.github.com>
2022-10-23 19:32:37 +0200

    fuzzing name

--- fuzzing/fuzzing_name.py
+++ fuzzing/fuzzing_name.py
@@ -19,9 +19,9 @@ def fuzzing(val):
             returned_value = get_correct_name()
         except AssertionError as msg:
             assert(msg == "le nom de l'utilisateur ne peut pas être un nombre"
-                    or msg == "name can't be less than 2 letter"
-                    or msg == "name can't be more than 20 letter")
-                    #or str(returned_value) == str(val))
+                    or msg == "le nom de l'utilisateur doit faire au moins 2 caractères"
+                    or msg == "le nom de l'utilisateur doit faire au plus 20 caractères")
+                    or returned_value == str(val))
 
 #fuzzing name
 atheris.Setup(sys.argv, fuzzing)
