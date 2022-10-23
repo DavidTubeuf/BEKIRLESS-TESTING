29a28e9ada257074a03263ee69064bab7f65593a
louisverdon <78412480+louisverdon@users.noreply.github.com>
2022-10-23 18:39:18 +0200

    fuzzing name

--- fuzzing/fuzzing_name.py
+++ fuzzing/fuzzing_name.py
@@ -1,3 +1,4 @@
+import unittest
 import atheris
 import mock
 import sys
@@ -10,19 +11,21 @@ sys.path.insert(0, parentdir)
 
 from Main import get_correct_name
 
-# Test the given name in input,
-def fuzzing(val):
-    with self.assertRaises(Exception) as cm:
-        with mock.patch('builtins.input', return_value=val):
-            returned_value = get_correct_name()
-    the_exception = cm.exception
-    res = str(the_exception)
-    assert(res == "le nom de l'utilisateur ne peut pas être un nombre"
-           or res == "name can't be less than 2 letter"
-           or res == "name can't be more than 20 letter"
-           or returned_value == val)
+class FuzzingTests(unittest.TestCase):
+    # Test the given name in input,
+    def fuzzing(val):
+        with self.assertRaises(Exception) as cm:
+            with mock.patch('builtins.input', return_value=val):
+                returned_value = get_correct_name()
+        the_exception = cm.exception
+        res = str(the_exception)
+        assert(res == "le nom de l'utilisateur ne peut pas être un nombre"
+               or res == "name can't be less than 2 letter"
+               or res == "name can't be more than 20 letter"
+               or returned_value == val)
 
-#fuzzing name
-atheris.Setup(sys.argv, fuzzing)
-atheris.instrument_all();
-atheris.Fuzz()
+if __name__ == '__main__':
+    f = FuzzingTests()
+    atheris.Setup(sys.argv, f.fuzzing)
+    atheris.instrument_all();
+    atheris.Fuzz()
